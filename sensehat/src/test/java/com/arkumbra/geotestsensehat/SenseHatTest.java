b5a1286a3cf6e81192795f3d80b42ba3e3396712
Luke G <lgardener1337@gmail.com>
2020-04-16 21:06:06 +0900

    Add joystick move test

--- sensehat/src/test/java/com/arkumbra/geotestsensehat/SenseHatTest.java
+++ sensehat/src/test/java/com/arkumbra/geotestsensehat/SenseHatTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import rpi.sensehat.api.Joystick;
 import rpi.sensehat.api.LEDMatrix;
 import rpi.sensehat.api.SenseHat;
+import rpi.sensehat.api.dto.Color;
 import rpi.sensehat.api.dto.JoystickEvent;
 import rpi.sensehat.api.dto.joystick.Direction;
 
@@ -22,6 +23,7 @@ public class SenseHatTest {
         LEDMatrix ledMatrix = senseHat.ledMatrix;
         Joystick joystick = senseHat.joystick;
 
+        ledMatrix.flipVertically();
         ledMatrix.lowLight(true);
 
         ledMatrix.showMessage("...");
@@ -31,6 +33,41 @@ public class SenseHatTest {
         ledMatrix.showMessage(direction.name());
     }
 
+    @Test
+    public void moveOnJoystickInput() {
+        SenseHat senseHat = new SenseHat();
+        LEDMatrix ledMatrix = senseHat.ledMatrix;
+        Joystick joystick = senseHat.joystick;
+
+        ledMatrix.flipVertically();
+        ledMatrix.lowLight(true);
+
+        ledMatrix.showMessage(".");
+        
+        int x = 3, y = 3;
+        
+        int counter = 0;
+
+        while (counter < 20) {
+            counter++;
+            ledMatrix.setPixel(x, y, Color.GREEN);
+            JoystickEvent joystickEvent = joystick.waitForEvent();
+            Direction direction = joystickEvent.getDirection();
+            switch (direction) {
+                case UP:    y++; break;
+                case DOWN:  y--; break;
+                case RIGHT: x++; break;
+                case LEFT:  x--; break;
+                default: continue;
+            }
+            
+            // clear events ?
+            joystick.getEvents();
+        }
+
+
+        ledMatrix.showMessage("End");
+    }
 
 
     private void waitFor(long ms) {
