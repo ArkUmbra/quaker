813dd7b537ba706b8dd06b0d250c372234ff3590
Luke G <lgardener1337@gmail.com>
2020-04-16 20:41:20 +0900

    Add joystick input test

--- sensehat/src/test/java/com/arkumbra/geotestsensehat/SenseHatTest.java
+++ sensehat/src/test/java/com/arkumbra/geotestsensehat/SenseHatTest.java
@@ -1,8 +1,11 @@
 package com.arkumbra.geotestsensehat;
 
 import org.junit.Test;
+import rpi.sensehat.api.Joystick;
 import rpi.sensehat.api.LEDMatrix;
 import rpi.sensehat.api.SenseHat;
+import rpi.sensehat.api.dto.JoystickEvent;
+import rpi.sensehat.api.dto.joystick.Direction;
 
 public class SenseHatTest {
 
@@ -11,9 +14,24 @@ public class SenseHatTest {
         LEDMatrix ledMatrix = new SenseHat().ledMatrix;
 
         ledMatrix.showMessage("Hello, World!");
-
-        waitFor(2000);
     }
+
+    @Test
+    public void waitForJoystickInput() {
+        SenseHat senseHat = new SenseHat();
+        LEDMatrix ledMatrix = senseHat.ledMatrix;
+        Joystick joystick = senseHat.joystick;
+
+        ledMatrix.lowLight(true);
+
+        ledMatrix.showMessage("...");
+        JoystickEvent joystickEvent = joystick.waitForEvent();
+        Direction direction = joystickEvent.getDirection();
+
+        ledMatrix.showMessage(direction.name());
+    }
+
+
 
     private void waitFor(long ms) {
         try {
